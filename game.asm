f73322513336073b554923c68d8cd4f4f054671a
nbenns <nigel@500px.com>
2020-11-22 19:37:17 -0500

    add knight sprite, display to the screen, move him around

--- game.asm
+++ game.asm
@@ -77,7 +77,79 @@ LoadPalettesLoop:
   STA P1Tile        ; tile number = 0
   STA P1Attr        ; color = 0, no flipping
 
-  LDA #PPUCTRL_V    ; enable NMI
+  LDA #$80
+  STA P1YPos+4        ; put sprite 0 in center ($80) of screen vert
+  LDA #$88
+  STA P1XPos+4        ; put sprite 0 in center ($80) of screen horiz
+  LDA #$02
+  STA P1Tile+4        ; tile number = 0
+  LDA #$00
+  STA P1Attr+4        ; color = 0, no flipping
+
+  LDA #$80
+  STA P1YPos+8        ; put sprite 0 in center ($80) of screen vert
+  LDA #$90
+  STA P1XPos+8        ; put sprite 0 in center ($80) of screen horiz
+  LDA #$04
+  STA P1Tile+8        ; tile number = 0
+  LDA #$00
+  STA P1Attr+8        ; color = 0, no flipping
+
+  LDA #$90
+  STA P1YPos+12      ; put sprite 0 in center ($80) of screen vert
+  LDA #$80
+  STA P1XPos+12       ; put sprite 0 in center ($80) of screen horiz
+  LDA #$06
+  STA P1Tile+12       ; tile number = 0
+  LDA #$00
+  STA P1Attr+12       ; color = 0, no flipping
+
+  LDA #$90
+  STA P1YPos+16      ; put sprite 0 in center ($80) of screen vert
+  LDA #$88
+  STA P1XPos+16       ; put sprite 0 in center ($80) of screen horiz
+  LDA #$08
+  STA P1Tile+16       ; tile number = 0
+  LDA #$00
+  STA P1Attr+16       ; color = 0, no flipping
+
+  LDA #$90
+  STA P1YPos+20      ; put sprite 0 in center ($80) of screen vert
+  LDA #$90
+  STA P1XPos+20       ; put sprite 0 in center ($80) of screen horiz
+  LDA #$0A
+  STA P1Tile+20       ; tile number = 0
+  LDA #$00
+  STA P1Attr+20       ; color = 0, no flipping
+
+  LDA #$A0
+  STA P1YPos+24      ; put sprite 0 in center ($80) of screen vert
+  LDA #$80
+  STA P1XPos+24       ; put sprite 0 in center ($80) of screen horiz
+  LDA #$0C
+  STA P1Tile+24       ; tile number = 0
+  LDA #$00
+  STA P1Attr+24       ; color = 0, no flipping
+
+  LDA #$A0
+  STA P1YPos+28      ; put sprite 0 in center ($80) of screen vert
+  LDA #$88
+  STA P1XPos+28       ; put sprite 0 in center ($80) of screen horiz
+  LDA #$0E
+  STA P1Tile+28       ; tile number = 0
+  LDA #$00
+  STA P1Attr+28       ; color = 0, no flipping
+
+  LDA #$A0
+  STA P1YPos+32      ; put sprite 0 in center ($80) of screen vert
+  LDA #$90
+  STA P1XPos+32       ; put sprite 0 in center ($80) of screen horiz
+  LDA #$10
+  STA P1Tile+32       ; tile number = 0
+  LDA #$00
+  STA P1Attr+32       ; color = 0, no flipping
+
+  LDA #(PPUCTRL_V | PPUCTRL_H)    ; enable NMI
   STA PPUCTRL
 
   LDX #PPUMASK_s    ; enable sprites
@@ -97,24 +169,56 @@ MOVEUP:
   AND #BUTTON_UP
   BEQ MOVEDOWN
   DEC P1YPos
+  DEC P1YPos+4
+  DEC P1YPos+8
+  DEC P1YPos+12
+  DEC P1YPos+16
+  DEC P1YPos+20
+  DEC P1YPos+24
+  DEC P1YPos+28
+  DEC P1YPos+32
 
 MOVEDOWN:
   LDA Joy1
   AND #BUTTON_DOWN
   BEQ MOVELEFT
   INC P1YPos
+  INC P1YPos+4
+  INC P1YPos+8
+  INC P1YPos+12
+  INC P1YPos+16
+  INC P1YPos+20
+  INC P1YPos+24
+  INC P1YPos+28
+  INC P1YPos+32
 
 MOVELEFT:
   LDA Joy1
   AND #BUTTON_LEFT
   BEQ MOVERIGHT
   DEC P1XPos
+  DEC P1XPos+4
+  DEC P1XPos+8
+  DEC P1XPos+12
+  DEC P1XPos+16
+  DEC P1XPos+20
+  DEC P1XPos+24
+  DEC P1XPos+28
+  DEC P1XPos+32
 
 MOVERIGHT:
   LDA Joy1
   AND #BUTTON_RIGHT
   BEQ MOVEDONE
   INC P1XPos
+  INC P1XPos+4
+  INC P1XPos+8
+  INC P1XPos+12
+  INC P1XPos+16
+  INC P1XPos+20
+  INC P1XPos+24
+  INC P1XPos+28
+  INC P1XPos+32
 
 MOVEDONE:
   LDA #SPRITEPAGE_LOW
@@ -129,7 +233,7 @@ MOVEDONE:
   .org $E000
 palette:
   .db $0F,$31,$32,$33,$0F,$35,$36,$37,$0F,$39,$3A,$3B,$0F,$3D,$3E,$0F
-  .db $0F,$1C,$15,$14,$0F,$02,$38,$3C,$0F,$1C,$15,$14,$0F,$02,$38,$3C
+  .db $0F,$00,$10,$20,$0F,$02,$38,$3C,$0F,$1C,$15,$14,$0F,$02,$38,$3C
 
   NOP
   .org $FFFA     ;first of the three vectors starts here
